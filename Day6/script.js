f435fe5a983e7d84d13d0cbede458035106af667
Aditya9600ghosh <ghoshaditya9600@gmail.com>
2024-10-14 13:06:42 +0530

    Day 6 JS

---
+++ Day6/script.js
@@ -0,0 +1,48 @@
+// let a = () => {
+//     console.log("Hello World");
+// }
+
+// let b=()=>console.log("Arrow function 2");
+
+
+// a();
+// b();
+
+
+// let c = (x,y)=>
+// {
+//     return x+y;
+// }
+// let res = c('Rahul','Ravi');
+// let res1 = c(12,32);
+// console.log(res,res1);
+
+
+// function display(){
+//     // let p1 = document.querySelector("p");
+
+//     let p1=document.querySelectorAll("p");
+//     p1[0].innerHTML="I am in paragraph";
+//     p1[0].style.backgroundColor="red";
+//     p1[1].innerHTML="I am in paragraph";
+//     p1[1].style.backgroundColor="green";
+
+// }
+
+// const changeheader = () => {
+//     let head1 = document.querySelector("h1");
+//     head1.textContent = "Change the title";
+//     let a1 = document.querySelector("a");
+//     a1.setAttribute("href","https://aktu.ac.in");
+//     a1.innerHTML = "AKTU WEBSITE";
+// };
+
+// let addcontent = () => {
+//     const newDiv = document.createElement('div');
+//     newDiv.textContent = 'This is a new div';
+
+//     const body = document.querySelector('body');
+//     body.appendChild(newDiv);
+// };
+
+
