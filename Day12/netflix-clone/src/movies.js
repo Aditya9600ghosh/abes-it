dedf7b3c5da832f2332ddd92906aca62f97e03ec
Aditya9600ghosh <ghoshaditya9600@gmail.com>
2024-11-18 12:58:00 +0530

    Basic Netflix Clone

---
+++ Day12/netflix-clone/src/movies.js
@@ -0,0 +1,74 @@
+const movies=[
+    {
+            id:1,
+            image :"https://rukminim2.flixcart.com/image/612/612/kxtaxzk0/movie/n/a/r/2016-dvd-sony-dadc-english-captain-america-civil-war-dvd-original-imaga6twxryu58fj.jpeg?q=70" ,
+            title:"Captain America: Civil War",
+            language:"English",
+            cost:435,
+    },
+    {
+        id:2,
+        image :"https://rukminim2.flixcart.com/image/612/612/ku5ufm80/movie/t/o/3/2020-3d-blu-ray-20th-century-fox-excel-home-videos-english-x-men-original-imag7ca7dtq8w4hn.jpeg?q=70" ,
+        title:"X-Men: Days Of Future Past",
+        language:"English",
+        cost:435,
+},
+{
+    id:3,
+    image :"https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70" ,
+    title:"Gangs Of Wasseypur I",
+    language:"English",
+    cost:360,
+},
+{
+    id:4,
+    image :"https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/b/5/avp2-original-imad7gyggyhezb8j.jpeg?q=70" ,
+    title:"Alien vs Predator 2",
+    language:"English",
+    cost:435,
+},
+{
+    id:5,
+    image :"https://rukminim2.flixcart.com/image/612/612/jm2c87k0/movie/j/v/m/2018-blu-ray-20th-century-fox-excel-home-videos-english-war-for-original-imaf9yvgpedf2wxj.jpeg?q=70" ,
+    title:"War for the Planet of the Apes",
+    language:"English",
+    cost:590,
+},
+{
+    id:6,
+    image :"https://rukminim2.flixcart.com/image/612/612/kxtaxzk0/movie/n/a/r/2016-dvd-sony-dadc-english-captain-america-civil-war-dvd-original-imaga6twxryu58fj.jpeg?q=70" ,
+    title:"Captain America: Civil War",
+    language:"English",
+    cost:435,
+},
+{
+    id:7,
+    image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/5/k/z/2023-blu-ray-universal-pictures-english-oppenheimer-original-imagwguyz8cvryfk.jpeg?q=70" ,
+    title:"The Mechanic",
+    language:"English",
+    cost:690,
+},
+{
+    id:8,
+    image :"https://rukminim2.flixcart.com/image/612/612/kxtaxzk0/movie/n/a/r/2016-dvd-sony-dadc-english-captain-america-civil-war-dvd-original-imaga6twxryu58fj.jpeg?q=70" ,
+    title:"Captain America: Civil War",
+    language:"English",
+    cost:435,
+},
+{
+    id:9,
+    image :"https://rukminim2.flixcart.com/image/612/612/klzhq4w0/movie/0/r/v/2021-blu-ray-excel-productions-audio-visuals-pvt-ltd-english-original-imagyzz5xvnnynzw.jpeg?q=70" ,
+    title:"Wonder Woman 1984",
+    language:"English",
+    cost:690,
+},
+{
+    id:10,
+    image :"https://rukminim2.flixcart.com/image/612/612/av-media/movies/a/5/x/monster-vs-aliens-3d-original-imad7vgjg7ypgnaz.jpeg?q=70" ,
+    title:"Monster VS Aliens",
+    language:"English",
+    cost:435,
+},
+];
+
+export default movies;
